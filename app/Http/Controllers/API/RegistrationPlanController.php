ff4dd57ab7889a4097cf7741b9281c9f8f41523c
nawar16 <nawarmk98@gmail.com>
2021-01-28 14:16:24 +0200

    registration plan

--- app/Http/Controllers/API/RegistrationPlanController.php
+++ app/Http/Controllers/API/RegistrationPlanController.php
@@ -20,15 +20,14 @@ class RegistrationPlanController extends Controller
         $reg = RegistrationPlan::select('id', 'study_year_semester_id','study_plan_id', 'faculty_id', 'department_id')
         ->with(['registrationCourses' => function($registrationCourse){
             $registrationCourse->with(['course' => function($course){
-                $course->select('id', 'code');
-            }, 'courseGroups' => function($courseGroup){
-                $courseGroup->select('name', 'capacity', 'registration_course_id')->with(['lectures' => function($lecture){
-                    $lecture->select('registration_course_group_id', 'day', 'start_time','end_time','place');
-                }]);
-            }, 'courseCategories' => function($courseCategorie){
-                $courseCategorie->select('name', 'capacity', 'registration_course_id')->with(['lectures' => function($lecture){
-                    $lecture->select('registration_course_category_id', 'day', 'start_time','end_time','place');
-                }]);
+                    $course->select('id', 'code');
+                }, 'courseGroups' => function($courseGroup){
+                    $courseGroup->select('name', 'capacity', 'registration_course_id')
+                    ->with('lectures');
+                }, 'courseCategories' => function($courseCategorie){
+                    $courseCategorie->select('name', 'capacity', 'registration_course_id')->with(['lectures' => function($lecture){
+                        $lecture->select('registration_course_category_id', 'day', 'start_time','end_time','place');
+                    }]);
             }])->select('id','registration_plan_id', 'course_id');
         }])
         ->find(1);
