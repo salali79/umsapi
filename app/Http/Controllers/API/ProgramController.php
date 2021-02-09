d753ee273679a21c69fed6b87707e010e9297dd6
Hanan Al-Slaiman <38842357+hananalsleman@users.noreply.github.com>
2021-02-09 15:41:49 +0200

    open hours

--- app/Http/Controllers/API/ProgramController.php
+++ app/Http/Controllers/API/ProgramController.php
@@ -6,18 +6,33 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Spatie\OpeningHours\OpeningHours;
 use App\Models\ProgramSchedule;
+use Auth;
+use Validator;
+use JWTFactory;
+use JWTAuth;
+use JWTAuthException;
 
 class ProgramController extends Controller
 {
 
+    function current_student(Request $request)
+    {
+        if(!is_null($request->lang)) app()->setLocale($request->lang);
+        $headers = apache_request_headers();
+        $request->headers->set('Authorization', $headers['Authorization']);
+        $token = $request->headers->get('Authorization');
+        JWTAuth::setToken($token);
+        $std = auth('student')->user();
+        return $std;
+    }
     public function add_course_time(Request $request)
     {
         $program = new ProgramSchedule();
         $program->student_id="3472";
-        $program->free_hours = [
-            'sunday' => 
+        $free_hours = [
+            'sunday' =>
              [$request['MOsun'].'-'.$request['MCsun'], $request['NOsun'].'-'.$request['NCsun']],
-            'monday' => 
+            'monday' =>
              [$request['MOmon'].'-'.$request['MCmon'], $request['NOmon'].'-'.$request['NCmon']],
             'tuesday' =>
              [$request['MOtus'].'-'.$request['MCtus'], $request['NOtus'].'-'.$request['NCtus']],
@@ -30,10 +45,12 @@ class ProgramController extends Controller
             'saturday' =>
              [$request['MOsat'].'-'.$request['MCsat'], $request['NOsat'].'-'.$request['NCsatn']],
         ];
+
+        $program->free_hours = json_encode($free_hours);
         /*if(count($opening) > 0){
             $program->update([
                 'free_hours' => $opening ? : null,
-             ]); 
+             ]);
         }*/
         $program->save();
     }
